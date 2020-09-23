6d3be504e484eec5e9aa7ea135d56c837f0ffe3d
Samantha Wray <samanthawray@berkeley.edu>
2020-09-23 10:59:23 -0700

    added get_accuracy and get_MCC

--- src/metrics.py
+++ src/metrics.py
@@ -43,12 +43,41 @@ def get_confusion_matrix2(seqlist, reflist):
 
 
 def get_accuracy(cmatrix):
-    pass
+    """Returns accuracy for a 2x2 confusion matrix.
+
+    Parameters
+    ----------
+        cmatrix : dict
+            Counts for true positives, true negatives, false positives, and
+            false negatives, keyed by TP, TN, FP, and FN, respectively.
+
+    Returns
+    -------
+        accuracy : float
+            Accuracy (frequency of correct classifications) from the cmatrix
+    """
+    return (cmatrix['TP'] + cmatrix['TN']) / sum(cmatrix.values())
 
 
 def get_MCC(cmatrix):
-    pass
+    """Returns MCC (Matthews correlation coefficient) for a 2x2 confusion matrix
 
+    Parameters
+    ----------
+        cmatrix : dict
+            Counts for true positives, true negatives, false positives, and
+            false negatives, keyed by TP, TN, FP, and FN, respectively.
+
+    Returns
+    -------
+        mcc : float
+            MCC (decimal between -1 and +1) indicating the quality of binary
+            classifications from cmatrix
+    """
+    tp, tn, fp, fn = cmatrix['TP'], cmatrix['TN'], cmatrix['FP'], cmatrix['FN']
+    numerator = tp * tn - fp * fn
+    denominator = ((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn)) or 1
+    return numerator / (denominator ** 0.5)
 
 def get_sensitivity(cmatrix):
     pass
