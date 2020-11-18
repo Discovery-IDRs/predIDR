6b3eb424671c4f2236b28c077c64f8c6edca133e
Marc Singleton <marcsingleton@berkeley.edu>
2020-11-18 01:00:15 -0800

    Consolidated features.py and completed feature_calc.py

--- analysis/models/feature_calc/feature_calc.py
+++ analysis/models/feature_calc/feature_calc.py
@@ -6,17 +6,24 @@ import features
 # Calculate features
 disprot_features = []
 for file_label in os.listdir('../../disprot_validation/extract_seqs/out/fastas/'):
+    disprot_id = file_label[:-6]
+    # Parse FASTA and get sequence
     with open(f'../../disprot_validation/extract_seqs/out/fastas/{file_label}') as file:
-        # Parse FASTA and get sequence
-        disprot_id = file_label[:-4]
-        seq = ''
+        file.readline()  # Skip header
+        lines = [line.rstrip() for line in file]
+        seq = ''.join(lines)
+    # Parse FASTA and get labels
+    with open(f'../../disprot_validation/extract_refs/out/fastas/{file_label}') as file:
+        file.readline()  # Skip header
+        lines = [line.rstrip() for line in file]
+        sym_labels = ''.join(lines)
 
-        # Apply feature functions to sequence
-        seq_features = features.get_features(seq, features.features, 15)
+    # Apply feature functions to sequence
+    seq_features = features.get_features(seq, features.features, 15)
 
-        # Store features in list labeled by DisProt ID and residue ID
-        for sym_id, sym_features in enumerate(seq_features):
-            disprot_features.append((disprot_id, str(sym_id), sym_features))
+    # Store features in list labeled by DisProt ID and residue ID
+    for sym_id, (sym_label, sym_features) in enumerate(zip(sym_labels, seq_features)):
+        disprot_features.append((disprot_id, str(sym_id), sym_label, sym_features))
 
 # Write features to file
 if not os.path.exists('out/'):
@@ -24,13 +31,15 @@ if not os.path.exists('out/'):
 
 with open('out/disprot_features.tsv', 'w') as file:
     feature_labels = list(features.features)
-    file.write('\t'.join(['disprot_id', 'sym_id'] + feature_labels) + '\n')
-    for disprot_id, sym_id, sym_features in disprot_features:
+    file.write('\t'.join(['disprot_id', 'sym_id', 'sym_label'] + feature_labels) + '\n')
+    for disprot_id, sym_id, sym_label, sym_features in disprot_features:
         values = [str(sym_features[feature_label]) for feature_label in feature_labels]
-        file.write('\t'.join([disprot_id, sym_id] + values) + '\n')
+        file.write('\t'.join([disprot_id, sym_id, sym_label] + values) + '\n')
 
 """
 DEPENDENCIES
+../../disprot_validation/extract_refs/extract_ref.py
+    ../../disprot_validation/extract_refs/out/fastas/*.fasta
 ../../disprot_validation/extract_seqs/extract_seq.py
     ../../disprot_validation/extract_seqs/out/fastas/*.fasta
 """
--- src/features.py
+++ src/features.py
@@ -1,6 +1,5 @@
 """Functions to calculate features associated with each amino acid in a protein sequence."""
 
-import re
 
 # Helper functions
 def get_window(seq, pos, window_size):
@@ -89,6 +88,7 @@ def get_X_fractions(seq, X, window_size):
         X_fractions.append(X_count / len(window))
     return X_fractions
 
+
 def get_repeat_count(seq):
     """Return count of symbols in seq which appear two or more times in a row.
 
@@ -102,35 +102,26 @@ def get_repeat_count(seq):
         repeat_count : int
             Count of repeat symbols in seq.
     """
-    repeat_count = 0
-
     if len(seq) <= 1:
         return 0
-    else:
-        pass
 
+    # Count terminal symbols
+    repeat_count = 0
     if seq[0] == seq[1]:
         repeat_count += 1
-    else:
-        pass
-
     if seq[len(seq) - 1] == seq[len(seq) - 2]:
         repeat_count += 1
-    else:
-        pass
 
+    # Count interior symbols
     if len(seq) > 2:
         for i in range(1, len(seq) - 1):
             if seq[i] == seq[i-1]:
                 repeat_count += 1
             elif seq[i] == seq[i+1]:
                 repeat_count += 1
-            else:
-                pass
-    else:
-        pass
 
     return repeat_count
+
 
 def get_repeat_fractions(seq, window_size):
     """Return fractions of symbols in sliding window across seq which appear two or more times in a row.
@@ -148,26 +139,23 @@ def get_repeat_fractions(seq, window_size):
         repeat_fractions : list
             Fractions of symbols in sliding window across seq which appear two or more times in a row.
     """
-
     repeat_fractions = []
-
     for i in range(len(seq)):
         window = get_window(seq, i, window_size)
         repeat_count = get_repeat_count(window)
         repeat_fractions.append(repeat_count / len(window))
-
     return repeat_fractions
 
+
 def get_pair_repeat_count(seq, XY):
-    """Return count of pair symbols contained in XY
-    in seq which appear two or more times in a row.
+    """Return count of pair symbols contained in XY in seq which appear two or more times in a row.
 
     Parameters
     ----------
         seq : string
             Protein sequence as string.
         XY : string or list
-            Pair symbols to count for repeats.
+            Pair of symbols to count for repeats.
             Must contain at least two symbols.
 
     Returns
@@ -175,33 +163,19 @@ def get_pair_repeat_count(seq, XY):
         pair_repeat_count : int
             Count of repeat pair symbols in seq.
     """
-
     if len(XY) < 2:
         raise ValueError('Requires at least two symbols.')
-
-    pair_repeat_count = 0
-
     if len(seq) <= 1:
         return 0
-    else:
-        pass
 
-    if seq[0] in XY:
-        if seq[1] in XY:
-            pair_repeat_count += 1
-        else:
-            pass
-    else:
-        pass
+    # Count terminal symbols
+    pair_repeat_count = 0
+    if seq[0] in XY and seq[1] in XY:
+        pair_repeat_count += 1
+    if seq[len(seq) - 1] in XY and seq[len(seq) - 2] in XY:
+        pair_repeat_count += 1
 
-    if seq[len(seq) - 1] in XY:
-        if seq[len(seq) - 2] in XY:
-            pair_repeat_count += 1
-        else:
-            pass
-    else:
-        pass
-
+    # Count interior symbols
     if len(seq) > 2:
         for i in range(1, len(seq) - 1):
             if seq[i] in XY:
@@ -209,14 +183,9 @@ def get_pair_repeat_count(seq, XY):
                     pair_repeat_count += 1
                 elif seq[i+1] in XY:
                     pair_repeat_count += 1
-                else:
-                    pass
-            else:
-                pass
-    else:
-        pass
 
     return pair_repeat_count
+
 
 def get_pair_repeat_fractions(seq, XY, window_size):
     """Return fractions of pair symbols contained in XY in sliding
@@ -227,8 +196,8 @@ def get_pair_repeat_fractions(seq, XY, window_size):
         seq : string
             Protein sequence as string.
         XY : string or list
-            Pair symbols to count for repeats.
-            Must contain at least two symbols.
+            Pair symbols to count for repeats. Must contain at least
+            two symbols.
         window_size : int
             Total number of symbols in window, including the center
             symbol. Must be an odd number.
@@ -236,36 +205,100 @@ def get_pair_repeat_fractions(seq, XY, window_size):
     Returns
     -------
         pair_repeat_fractions : list
-            Fractions of pair symbols in sliding window across
-            seq which appear two or more times in a row.
+            Fractions of pair symbols in sliding window across seq
+            which appear two or more times in a row.
     """
-
     pair_repeat_fractions = []
-
     for i in range(len(seq)):
         window = get_window(seq, i, window_size)
         pair_repeat_count = get_pair_repeat_count(window, XY)
         pair_repeat_fractions.append(pair_repeat_count / len(window))
-
     return pair_repeat_fractions
 
-def get_regex_count(seq, regex):
-    """Return count of patterns matching regex in seq."""
-    pattern = re.compile(regex)
-    count = pattern.findall(seq)
-    return len(count)
+
+def get_hydrophobicity(seq):
+    """Return average hydrophobicity of symbols in seq.
+
+    Parameters
+    ----------
+        seq : string
+            Protein sequence as string.
+
+    Returns
+    -------
+        hydrophobicity : int
+            Score of hydrophobicity with most hydrophobic at 1 and
+            most hydrophilic at 0.
+    """
+    hydrophobicity_dict = {'I': 4.5, 'V': 4.2, 'L': 3.8, 'F': 2.8, 'C': 2.5,
+                           'M': 1.9, 'A': 1.8, 'W': -0.9, 'G': -0.4, 'T': -0.7,
+                           'S': -0.8, 'Y': -1.3, 'P': -1.6, 'H': -3.2, 'N': -3.5,
+                           'D': -3.5, 'Q': -3.5, 'E': -3.5, 'K': -3.9, 'R': -4.5}
+    hydrophobicity_dict = {key: (value + 4.5) / 9 for key, value in hydrophobicity_dict.items()}
+    seq_hydrophobicities = [hydrophobicity_dict.get(sym, 0) for sym in seq]
+    return sum(seq_hydrophobicities) / len(seq_hydrophobicities)
 
 
-def get_regex_fractions(seq, regex, window):
-    """Return fractions of patterns matching X in sliding window across seq."""
-    if get_regex_count(seq,regex) == 0:
-        return float("NaN")
-    fraction = get_regex_count(window,regex)/get_regex_count(seq,regex)
-    return fraction
+def get_polarity(seq):
+    """Return average polarity of symbols in seq.
+
+    Parameters
+    ----------
+        seq : string
+            Protein sequence as string.
+
+    Returns
+    -------
+        polarity : int
+            Score of average polarity ranging from 0 to 1, where 1 is
+            polar and 0 is nonpolar.
+    """
+    polarity_dict = {'I': 0, 'V': 0, 'L': 0, 'F': 0, 'C': 0, 'M': 0, 'A': 0,
+                     'W': 0, 'G': 0, 'T': 1, 'S': 1, 'Y': 1, 'P': 0, 'H': 1,
+                     'N': 1, 'D': 1, 'Q': 1, 'E': 1, 'K': 1, 'R': 1}
+    seq_polarities = [polarity_dict.get(sym, 0.5) for sym in seq]
+    return sum(seq_polarities) / len(seq_polarities)
 
 
+def get_content_scores(seq, content_func, window_size):
+    """Return amino acid content score (scaled to 0-1) of amino acids in seq.
 
-# TODO: Not all features will necessarily accept a window_size parameter, so we may need to rework this
+    Parameters
+    ----------
+        seq : string
+            Protein sequence as string.
+        content_func : function
+            Gives content score for a specified sequence, ranging from 0 to 1.
+        window_size : int
+            Total number of symbols in window, including the center
+            symbol. Must be an odd number.
+
+    Returns
+    -------
+        content_scores : list
+            Scores of content in sliding window across seq
+    """
+    content_scores = []
+    for i in range(len(seq)):
+        window = get_window(seq, i, window_size)
+        score = content_func(window)
+        content_scores.append(score)
+    return content_scores
+
+
+def AA_score(seq, ref, window_size):
+    """Return list of average scores based on a reference dictionary."""
+    scores = []
+    for i in range(0, len(seq)):
+        window = get_window(seq, i, window_size)
+        window_scores = []
+        for sym in window:
+            if sym in ref:
+                window_scores.append(ref[sym])
+        scores.append(sum(window_scores) / len(window_scores))
+    return scores
+
+
 def get_features(seq, features, window_size):
     """Return values for each feature in features at each symbol in seq."""
     label2idx = {}  # Dictionary of (lists of feature values) keyed by feature label
@@ -273,14 +306,29 @@ def get_features(seq, features, window_size):
         label2idx[feature_label] = function(seq, window_size)
     feature_labels = list(features)
     idx2label = []  # List of (dictionaries of feature values keyed by feature label)
-    for i in range(len(seq)):
+    for i in range(len(seq)-1):
         idx2label.append({feature_label: label2idx[feature_label][i] for feature_label in feature_labels})
     return idx2label
 
 
+# Reference dictionary for isoelectric point (average pKa)
+AA_PI_value = {"G": 5.97, "A": 6.00, "V": 5.96, "L": 5.98, "I": 6.02, "M": 5.74, "P": 6.30, "F": 5.48,
+               "W": 5.89, "N": 5.41, "Q": 5.65, "S": 5.68, "T": 5.60, "Y": 5.66, "C": 5.07, "D": 2.77,
+               "E": 3.22, "K": 9.74, "R": 10.76, "H": 7.59}
+
+# Reference dictionary of binary values based on the acidity/basicity of side chains
+AA_acidic = {'D': 1, 'E': 1, 'K': 0, 'R': 0, 'H': 0, 'G': 0, 'A': 0, 'V': 0, 'I': 0, 'W': 0, 'F': 0, 'P': 0, 'M': 0,
+             'L': 0, 'S': 0, 'T': 0, 'Y': 0, 'N': 0, 'Q': 0, 'C': 0}
+AA_basic = {'D': 0, 'E': 0, 'K': 1, 'R': 1, 'H': 1, 'G': 0, 'A': 0, 'V': 0, 'I': 0, 'W': 0, 'F': 0, 'P': 0, 'M': 0,
+            'L': 0, 'S': 0, 'T': 0, 'Y': 0, 'N': 0, 'Q': 0, 'C': 0}
+
 # Feature functions and feature dictionary
-features = {'fraction_AGP': lambda seq, window_size: get_X_fractions(seq, 'AGP', window_size),
-            'fraction_SEG': lambda seq, window_size: get_X_fractions(seq, 'SEG', window_size),
+features = {'hydrophobicity_content': lambda seq, window_size: get_content_scores(seq, get_hydrophobicity, window_size),
+            'polarity_content': lambda seq, window_size: get_content_scores(seq, get_polarity, window_size),
+            'aromatic_content': lambda seq, window_size: get_X_fractions(seq, 'FYWH', window_size),
+            'acidic_content': lambda seq, window_size: AA_score(seq, AA_acidic, window_size),
+            'basic_content': lambda seq, window_size: AA_score(seq, AA_basic, window_size),
+            'isoelectric point': lambda seq, window_size: AA_score(seq, AA_PI_value, window_size),
             'fraction_pos_charge': lambda seq, window_size: get_X_fractions(seq, 'KRH', window_size),
             'fraction_neg_charge': lambda seq, window_size: get_X_fractions(seq, 'DE', window_size),
             'fraction_repeat': lambda seq, window_size: get_repeat_fractions(seq, window_size),
@@ -311,37 +359,5 @@ features = {'fraction_AGP': lambda seq, window_size: get_X_fractions(seq, 'AGP',
             'fraction_K': lambda seq, window_size: get_X_fractions(seq, 'K', window_size),
             'fraction_R': lambda seq, window_size: get_X_fractions(seq, 'R', window_size),
             'fraction_H': lambda seq, window_size: get_X_fractions(seq, 'H', window_size),
-            'fraction_?': lambda seq, window_size: get_X_fractions(seq, '?', window_size)}
-#the reference dictionary for isoelectric point (average pka)
-AA_PI_value = {"G": 5.97, "A":6, "V": 5.96, "L":5.98, "I":6.02, "M": 5.74, "P": 6.30, "F":5.48, 
-              "W":5.89, "N":5.41, "Q":5.65, "S":5.68, "T":5.60, "Y":5.66, "C": 5.07, "D": 2.77, 
-               "E":3.22, "K":9.74, "R":10.76, "H": 7.59}
-
-#assigned binary values based on the acidity/basicity of side chains
-AA_Acid= {'D':1, 'E':1, 'K':0, 'R':0, 'H':0, 'G': 0, 'A':0, 'V':0, 'I':0, 'W':0, 'F':0, 'P':0, 'M':0, 
-                            'L':0, 'S':0, 'T':0, 'Y':0, 'N':0, 
-                            'Q':0, 'C':0}
-AA_Basic= {'D':0, 'E':0, 'K':1, 'R':1, 'H':1, 'G': 0, 'A':0, 'V':0, 'I':0, 'W':0, 'F':0, 'P':0, 'M':0, 
-                            'L':0, 'S':0, 'T':0, 'Y':0, 'N':0, 
-                            'Q':0, 'C':0}
-
-
-def AA_Score(seq, ref, window_size):
-    """computes a list of average scores based on a reference dictionary"""
-    scores = []
-    sequences = []
-    for i in list(range(0, len(seq),window_size)):
-        target_seq = get_window(seq, i, window_size)
-        sequences.append([target_seq])
-    for window in sequences:
-        score = 0
-        window = [a for a in window[0]]
-        for aa in window:
-            score += ref.get(aa)
-        scores.append(score)
-    return scores
-
-features = {'acid_content': lambda seq, window_size: AA_Score(seq, AA_Acid, window_size),
-            'basic_content':lambda seq, window_size: AA_Score(seq, AA_Basic, window_size),
-            'isoelectric point': lambda seq, window_size: AA_Score(seq, AA_PI_value, window_size)
-            }
+            'fraction_AGP': lambda seq, window_size: get_X_fractions(seq, 'AGP', window_size),
+            'fraction_SEG': lambda seq, window_size: get_X_fractions(seq, 'SEG', window_size)}
