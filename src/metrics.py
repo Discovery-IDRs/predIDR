ec18ef2eac566c8153e91db376507bde4c391cc2
Ananya <ananyaks@berkeley.edu>
2021-02-17 16:52:02 -0800

    changed some metrics to sklearn implementation

--- src/metrics.py
+++ src/metrics.py
@@ -1,178 +1,56 @@
 import seaborn as sns
 import matplotlib
 import matplotlib.pyplot as plt
+import itertools
 
 from sklearn.metrics import *
 
 """Functions to calculate metrics for evaluating the performance of classifiers."""
 
+def get_confusion_matrix1(y_true, y_pred_bin):
+    """Returns 2x2 confusion matrix for a single sequence."""
+    check_inputs_valid(y_true, y_pred_bin)
+    cm = confusion_matrix(y_true, y_pred_bin)
+    return cm
 
-def get_confusion_matrix1(seq, ref):
-    """Returns 2x2 confusion matrix for a single sequence.
+def get_confusion_matrix2(y_true_list, y_pred_bin_list):
+    """Returns 2x2 confusion matrix for a list of sequences."""
+    y_true = list(itertools.chain.from_iterable(y_true_list))
+    y_pred_bin = list(itertools.chain.from_iterable(y_pred_bin_list))
+    
+    check_inputs_valid(y_true, y_pred_bin)
+    cm = confusion_matrix(y_true, y_pred_bin)
+    return cm
 
-    Parameters
-    ----------
-        seq : list
-            Predicted labels for each residue, ordered as in the original
-            sequence. Assumes 0 == not disordered, 1 == disordered.
-        ref : list
-            Actual labels for each residue, ordered as in the original sequence.
+def get_accuracy(y_true, y_pred_bin)):
+    """Returns accuracy for a 2x2 confusion matrix."""
+    return accuracy_score(y_true, y_pred_bin)
 
-    Returns
-    -------
-        cmatrix : dict
-            Counts for true positives, true negatives, false positives, and
-            false negatives, keyed by TP, TN, FP, and FN, respectively.
-    """
-    check_inputs_valid(seq, ref)
-
-    TP, FP, TN, FN = 0, 0, 0, 0
-
-    ref_counter = 0
-    for s in seq:
-        r = ref[ref_counter]
-        if s == 0:
-            if r == s:
-                TN += 1
-            else:
-                FN += 1
-        elif s == 1:
-            if r == s:
-                TP += 1
-            else:
-                FP += 1
-        ref_counter += 1
-
-    cmatrix = {"TP": TP,
-               "FP": FP,
-               "TN": TN,
-               "FN": FN}
-
-    return cmatrix
-
-
-def get_confusion_matrix2(seqlist, reflist):
-    """Returns 2x2 confusion matrix for a list of sequences.
-
-    Parameters
-    ----------
-        seqlist : list of lists
-            List of predicted labels for each residue, ordered as in the
-            original sequence.
-        reflist : list of lists
-            List of actual labels for each residue, ordered as in the original
-            sequence.
-
-    Returns
-    -------
-        cmatrix : dict
-            Counts for true positives, true negatives, false positives, and
-            false negatives, keyed by TP, TN, FP, and FN, respectively.
-    """
-
-    TP, FP, TN, FN = 0, 0, 0, 0
-    ref_counter = 0
-
-    for s in seqlist:
-        r = reflist[ref_counter]
-        check_inputs_valid(s, r)
-        dict = get_confusion_matrix1(s, r)
-        TP += dict.get("TP")
-        FP += dict.get("FP")
-        TN += dict.get("TN")
-        FN += dict.get("FN")
-        ref_counter += 1
-
-    cmatrix = {"TP": TP,
-               "FP": FP,
-               "TN": TN,
-               "FN": FN}
-
-    return cmatrix
-
-
-def get_accuracy(cmatrix):
-    """Returns accuracy for a 2x2 confusion matrix.
-
-    Parameters
-    ----------
-        cmatrix : dict
-            Counts for true positives, true negatives, false positives, and
-            false negatives, keyed by TP, TN, FP, and FN, respectively.
-
-    Returns
-    -------
-        accuracy : float
-            Accuracy (frequency of correct classifications) from the cmatrix.
-    """
-    return (cmatrix["TP"] + cmatrix["TN"]) / sum(cmatrix.values())
-
-
-def get_MCC(cmatrix):
-    """Returns MCC (Matthews correlation coefficient) for a 2x2 confusion matrix.
-
-    Parameters
-    ----------
-        cmatrix : dict
-            Counts for true positives, true negatives, false positives, and
-            false negatives, keyed by TP, TN, FP, and FN, respectively.
-
-    Returns
-    -------
-        mcc : float
-            MCC (decimal between -1 and +1) indicating the quality of binary
-            classifications from cmatrix.
-    """
-    TP, TN, FP, FN = cmatrix["TP"], cmatrix["TN"], cmatrix["FP"], cmatrix["FN"]
-    numerator = TP * TN - FP * FN
-    denominator = ((TP + FP) * (TP + FN) * (TN + FP) * (TN + FN)) or 1
-    if denominator == 0:
-        return float("NaN")
-    return numerator / (denominator ** 0.5)
-
+def get_MCC(y_true, y_pred_bin):
+    """Returns MCC (Matthews correlation coefficient) for a 2x2 confusion matrix."""
+    return matthews_corrcoef(y_true, y_pred_bin)
 
 def get_sensitivity(cmatrix):
-    """Returns sensitivity for a 2x2 confusion matrix.
-
-       Parameters
-       ----------
-           cmatrix : dict
-               Counts for true positives, true negatives, false positives, and
-               false negatives, keyed by TP, TN, FP, and FN, respectively.
-
-       Returns
-       -------
-           sensitivity : float
-               Measures the proportion of positives that are correctly identified.
-    """
-    numerator = cmatrix["TP"]
-    denominator = (cmatrix["TP"] + cmatrix["FN"])
-    if denominator == 0:
-        return float("NaN")
-    return numerator/denominator
-
+    """Returns sensitivity for a 2x2 confusion matrix."""
+    return recall_score(y_true, y_pred_bin)
 
 def get_specificity(y_true, y_pred_bin):
     """Returns specificity (true negative rate) for binary classification."""
-    cm = confusion_matrix(y_true, y_pred_bin) # exchange for our cm func later
+    cm = get_confusion_matrix1(y_true, y_pred_bin) 
     tn, fp, fn, tp = confusion_matrix([0, 1, 0, 1], [1, 1, 1, 0]).ravel()
     return tn / (tn + fp)
-
 
 def get_precision(y_true, y_pred_bin):
     """Returns precision for binary classification."""
     return precision_score(y_true, y_pred_bin, zero_division=0)
 
-
 def get_f1(y_true, y_pred_bin):
     """Returns f1 score for binary classification."""
     return f1_score(y_true, y_pred_bin, zero_division=0)
 
-
 def get_AUC(y_true, y_pred_dec):
     """Returns AUC for decimal classification."""
     return roc_auc_score(y_true, y_pred_dec)
-
 
 def get_cross_entropy(y_true, y_pred_dec):
     """Returns cross entropy (log loss) for decimal predictions."""
@@ -278,9 +156,10 @@ def get_metrics(y_true, y_pred, visual=False):
 
 def get_binary_metrics(y_true, y_pred_bin):
     """Return metrics with binary classification."""
+
     return
 
-def get_decimal_metrics()y_true, y_pred:
+def get_decimal_metrics(y_true, y_pred):
     """Return metrics with decimal classification."""
     return
 
