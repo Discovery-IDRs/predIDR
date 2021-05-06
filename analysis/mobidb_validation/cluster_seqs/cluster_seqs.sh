932a535b9c8d6712972ad236412f1ccb8aeb08e1
Marc Singleton <marcsingleton@berkeley.edu>
2021-05-06 17:55:03 -0400

    Converted all notebooks into Python scripts and streamlined various aspects

--- analysis/mobidb_validation/cluster_seqs/cluster_seqs.sh
+++ analysis/mobidb_validation/cluster_seqs/cluster_seqs.sh
@@ -4,10 +4,14 @@ if [ ! -d out/ ]; then
   mkdir out/
 fi
 
-../../../bin/cd-hit -i ../remove_outliers/out/allseq.fasta -o out/allseq -c 0.4 -n 2 -d 0 -g 1
+../../../bin/cd-hit -i ../remove_outliers/out/mobidb_seqs.fasta -o out/mobidb -c 0.4 -n 2 -d 0 -g 1
 
 # NOTES
 # c sequence identity threshold
 # n word_length (2 as recommended for low identity)
 # d full header up to first space in .clstr file description
 # g accurate mode: find best matching cluster
+#
+# DEPENDENCIES
+# ../remove_outliers/remove_outliers.py
+#     ../remove_outliers/out/mobidb_seqs.fasta
--- analysis/mobidb_validation/remove_outliers/remove_outliers.py
+++ analysis/mobidb_validation/remove_outliers/remove_outliers.py
@@ -1,61 +1,63 @@
 """Remove outliers from data."""
 
 import os
-
 import scipy.ndimage as ndimage
-from Bio import SeqIO
 
-# Load outlier IDs
-outlier_ids = ['Q8WZ42', 'O97791', 'G4SLH0']  # Titin sequences
+
+def load_fasta(path):
+    fasta = []
+    with open(path) as file:
+        line = file.readline()
+        while line:
+            if line.startswith('>'):
+                header = line
+                line = file.readline()
+
+            seqlines = []
+            while line and not line.startswith('>'):
+                seqlines.append(line.rstrip())
+                line = file.readline()
+            seq = ''.join(seqlines)
+            fasta.append((header, seq))
+    return fasta
+
+
+# Load outlier accessions
+outliers = {'Q8WZ42', 'O97791', 'G4SLH0'}  # Titin sequences
 with open('../mobidb_stats/out/ns_codes.tsv') as file:  # Sequences with non-standard amino acids
     file.readline()  # Skip header
     for line in file:
         fields = line.split('\t')
-        outlier_ids.append(fields[0])
+        outliers.add(fields[0])
 
-# Make output directoies
+# Make output directories
 if not os.path.exists('out/'):
     os.mkdir('out/')
-if not os.path.exists('out/Seq/'):
-    os.mkdir('out/Seq/')
-if not os.path.exists('out/Disorder/'):
-    os.mkdir('out/Disorder/')
 
-# Remove outliers from allseq
-fastas = SeqIO.parse('../generate_fastas/out/allseq.fasta', 'fasta')
-with open('out/allseq.fasta', 'w') as allseq:
-    for record in fastas:
-        id = record.id.split('|')[0]
-        if record.id.split('|')[0] not in outlier_ids:
-            # Write to allseq.fasta
-            seq = str(record.seq)
+# Remove outliers from seqs
+fastas = load_fasta('../generate_fastas/out/mobidb_seqs.fasta')
+with open('out/mobidb_seqs.fasta', 'w') as file:
+    for header, seq in fastas:
+        accession = header.split('|')[0][1:]  # Trim >
+        if accession not in outliers:
             seqstring = '\n'.join(seq[i:i + 80] for i in range(0, len(seq), 80))
-            allseq.write('>' + record.description + '\n' + seqstring + '\n')
+            file.write(header + seqstring + '\n')
 
-            # Write to individual fasta
-            with open(f'out/Seq/{id}_seq.fasta', 'w') as oneseq:
-                oneseq.write('>' + record.description + '\n' + seqstring + '\n')
-
-# Remove outliers alldisorder including flipping labels in short segments
-fastas = SeqIO.parse('../generate_fastas/out/alldisorder.fasta', 'fasta')
-with open('out/alldisorder.fasta', 'w') as allseq:
-    for record in fastas:
-        id = record.id.split('|')[0]
-        if record.id.split('|')[0] not in outlier_ids:
-            # Write to alldisorder.fasta
-            seq1 = [int(s) for s in str(record.seq)]
+# Remove outliers from labels including flipping labels in short segments
+fastas = load_fasta('../generate_fastas/out/mobidb_labels.fasta')
+with open('out/mobidb_labels.fasta', 'w') as file:
+    for header, seq in fastas:
+        accession = header.split('|')[0][1:]  # Trim >
+        if accession not in outliers:
+            seq1 = [int(sym) for sym in str(seq)]
             seq2 = ['0' for _ in range(len(seq1))]
             labels = ndimage.label(seq1)[0]
             for s, in ndimage.find_objects(labels):  # Unpack 1-tuple
                 if s.stop - s.start >= 10:
                     seq2[s.start:s.stop] = (s.stop-s.start) * ['1']
-            seq2 = ''.join(seq2)
-            seqstring = '\n'.join(seq2[i:i + 80] for i in range(0, len(seq2), 80))
-            allseq.write('>' + record.description + '\n' + seqstring + '\n')
-
-            # Write to individual fasta
-            with open(f'out/Disorder/{id}_disorder.fasta', 'w') as oneseq:
-                oneseq.write('>' + record.description + '\n' + seqstring + '\n')
+            seq = ''.join(seq2)
+            seqstring = '\n'.join(seq[i:i + 80] for i in range(0, len(seq), 80))
+            file.write(header + seqstring + '\n')
 
 """
 NOTES
@@ -65,4 +67,10 @@ Short disordered segments (9 aa or smaller) were also removed by flipping their 
     This was largely motivated by the exclusion such segments from DisProt as well as their tendency to be simple loops
     connecting larger, ordered domains.
     Proteins with no disordered labels following this change were not removed.
+
+DEPENDENCIES
+../generate_fastas/generate_fastas.py
+    ../generate_fastas/out/*
+../mobidb_stats/mobidb_stats.py
+    ../mobidb_stats/mobidb_stats/out/ns_codes.tsv
 """
