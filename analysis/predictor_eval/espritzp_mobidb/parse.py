c73cdc8b6e1874b7d91d53afc713188e11ad8ac8
Marc Singleton <marcsingleton@berkeley.edu>
2021-06-15 13:44:36 -0700

    Added espritzp_mobidb/

---
+++ analysis/predictor_eval/espritzp_mobidb/parse.py
@@ -0,0 +1,30 @@
+"""Parse raw output from Espritz into combined FASTA file."""
+
+import os
+
+with open('out/espritzs_labels.fasta', 'w') as labels:
+    paths = [path for path in os.listdir('out/raw/') if path.endswith('espritz')]
+    for path in sorted(paths):
+        accession = path.split('.')[0]
+        with open(f'out/raw/{accession}.espritz') as file:
+            star_count = 0
+            label = []
+            for line in file:
+                if line.startswith('*'):
+                    star_count += 1
+                elif star_count >= 2:
+                    sym = '1' if float(line.split()[1]) >= 0.2644 else '0'  # Cutoff was pulled from espritz code; unclear if threshold > or >=, so may differ in edge cases
+                    label.append(sym)
+            label = ''.join(label)
+            labelstring = '\n'.join([label[i:i+80] for i in range(0, len(label), 80)]) + '\n'
+
+        # Write labels to file
+        with open(f'../../mobidb_validation/format_seqs/out/seqs/{accession}.fasta') as file:
+            header = file.readline()
+        labels.write(header + labelstring)
+
+"""
+DEPENDENCIES
+../../mobidb_validation/format_seqs/format_seqs.py
+./predict.sh
+"""
