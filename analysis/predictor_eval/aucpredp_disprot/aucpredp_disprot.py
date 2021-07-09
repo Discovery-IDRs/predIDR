62773e5f4f8dd52e0441ddf459831daff6c7e91c
Marc Singleton <marcsingleton@berkeley.edu>
2021-07-08 19:08:11 -0700

    Fixed aucpredp_disprot so it does not write empty record in FASTA due to bug in AUCpred execution

--- analysis/predictor_eval/aucpredp_disprot/aucpredp_disprot.py
+++ analysis/predictor_eval/aucpredp_disprot/aucpredp_disprot.py
@@ -42,10 +42,25 @@ if __name__ == '__main__':
 
     with open('out/aucpredp_scores.fasta', 'w') as scores, open('out/aucpredp_labels.fasta', 'w') as labels:
         for header, scorestring, labelstring in sorted(records):
+            if 'DP02925' in header:
+                continue
             scores.write(header + scorestring)
             labels.write(header + labelstring)
 
 """
+NOTES
+DP02925 does not complete successfully, returning the error message:
+
+seq_len 7096 not equal to reso_len 4084
+
+The length of the sequence is 7096, so I suspect one portion of the TGT file is poorly formatted or complete. The format
+is not documented at all, so it is difficult to say where the error is. It appears it's simply the outputs of search
+programs like PSI-BLAST or predictors like DISOPRED concatenated, one after the other.
+
+Unfortunately, the predictor makes empty files rather than terminating, so when the raw outputs are compiled, DP02925
+has an "empty" record (header with no corresponding sequence). The empty entries for in the compiled label and score
+FASTA files are therefore manually removed.
+
 DEPENDENCIES
 ../tgt_disprot/tgt_disprot.py
 """
