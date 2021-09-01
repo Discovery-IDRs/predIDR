9e9b1e4845ca0ce45356a98288a81621fdbac4ee
Marc Singleton <marcsingleton@berkeley.edu>
2021-09-01 09:23:07 -0700

    Added mobidb-pdb_validation/cluster_seqs/

---
+++ analysis/mobidb-pdb_validation/cluster_seqs/cluster_seqs.sh
@@ -0,0 +1,17 @@
+# Cluster seqs at 40% sequence identity
+
+if [ ! -d out/ ]; then
+  mkdir out/
+fi
+
+../../../bin/cd-hit -i ../remove_outliers/out/mobidb-pdb_seqs.fasta -o out/mobidb-pdb -c 0.4 -n 2 -d 0 -g 1
+
+# NOTES
+# c sequence identity threshold
+# n word_length (2 as recommended for low identity)
+# d full header up to first space in .clstr file description
+# g accurate mode: find best matching cluster
+#
+# DEPENDENCIES
+# ../remove_outliers/remove_outliers.py
+#     ../remove_outliers/out/mobidb-pdb_seqs.fasta
