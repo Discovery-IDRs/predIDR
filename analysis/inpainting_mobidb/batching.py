6718b68072a63393d8b91eba3198b4057d031879
Samiha Mahin <samihamahin@berkeley.edu>
2022-01-12 14:02:18 -0800

    updated batching.py to match Marc updates and corrections

--- analysis/inpainting_mobidb/batching.py
+++ analysis/inpainting_mobidb/batching.py
@@ -1,366 +1,256 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-#!pip install tensorflow
-
-
-# In[1]:
-
-
 import Bio.SeqIO as SeqIO
 import numpy as np
-import tensorflow
-from tensorflow import keras
-import matplotlib.pyplot as plt
-
-
-# In[2]:
-
+import tensorflow as tf
 
 # Parameters
+BATCH_NUM = 10
 sym_codes = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L',
              'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
+sym2idx = {i: sym for i, sym in enumerate(sym_codes)}
+train_seq_path = 'out/unmasked_seq_file.fasta'
+train_label_path = 'out/label.fasta'
 
 
-# In[3]:
+# Utility functions
+def load_data(seq_path, label_path):
+    """Return sequences and labels from FASTA files as arrays.
 
-
-BATCH_SIZE = 10 
-
-
-# In[4]:
-
-
-# Helper function for func load_data 
-def convert_ohc(seq):
+    :param seq_path: path for FASTA file of amino acid sequences
+    :param label_path: path for FASTA file of labels of amino acid sequences where disordered residues are labeled are labeled as 1 and ordered residues are labeled as 0
+    :return: array of one hot encoded sequences and array of labels
     """
-    One hot encodes given amino acid sequence string.
-    
-    :param seq: string of amino acid sequence 
-    :return: 2D array of one hot encoded string 
-    
-    """
-    seq_idx = [sym_codes.index(sym) for sym in seq]
-    x = np.array(seq_idx)
-    x = keras.utils.to_categorical(x, num_classes=len(sym_codes), dtype='int32')
-    
-    return x
+    ohes = []
+    labels = []
+    for record_seq, record_label in zip(SeqIO.parse(seq_path, 'fasta'), SeqIO.parse(label_path, 'fasta')):
+        # Convert string seq to one-hot-encoded seq
+        ohe = seq_to_OHE(str(record_seq.seq))
+        ohes.append(ohe)
 
-
-# In[5]:
-
-
-def load_data(seqs_path, label_path):
-    """
-    Loads sequences and lables from fasta files. 
-    
-    :param seq_path: path for fasta file of amino acid sequences 
-    :param label_path: path fasta file of labels of amino acid sequences where disordered residues are labeled are labeled as 1 and ordered residues are labeled as 0
-    :return: array all one hot encoded sequences and array of all labels from 
-    """
-    seq_ohc_lst = []
-    label_lst = []
-    
-    for record_seq, record_label in zip(SeqIO.parse(seqs_path, 'fasta'), SeqIO.parse(label_path, 'fasta')):
-        
-        # one hot encode each record_seq 
-        seq = str(record_seq.seq)
-        seq_ohc = convert_ohc(seq)
-        seq_ohc_lst.append(seq_ohc)
-        
-        # expand the dimension of record_label for broadcasting
+        # Convert string label to int label
         label = [int(sym) for sym in record_label]
-        label_lst.append(label)
-        
-    return np.array(seq_ohc_lst), np.array(label_lst)
+        labels.append(label)
+
+    return np.array(ohes), np.array(labels)
 
 
-# In[6]:
+def get_context_weight(ohe, label):
+    """Return context and weight from one-hot-encoded sequences and labels.
 
-
-def get_weight_target_context(seq_ohc, label):
+    :param ohe: one hot ended 2D arrays of sequences
+    :param label: array of labels corresponding to seq_ohc
+    :return: target, context and weight according to seq_ohc and label
     """
-    Gets the target, context, and weight from one hot encoded sequences and labels. 
-    
-    :param seq_ohc: one hot ended 2D arrays of sequences 
-    :param label: array of labels corresponding to seq_ohc 
-    :return: target, context and weight according to seq_ohc and label 
-    
+    weight = np.expand_dims(label, axis=2)
+    context = (np.invert(weight) + 2) * ohe
+
+    return context, weight
+
+
+def seq_to_OHE(seq):
+    """Return sequence as one-hot-encoded vector
+
+    :param seq: amino acid sequence as string
+    :return: one-hot-encoded string as 2D array
     """
-
-    weight = np.expand_dims(label, axis = 2)
-
-    # get the target from the record 
-    target = weight*seq_ohc
-        
-    # get the context from the record (inverted the weight)
-    context = (np.invert(weight) + 2)*seq_ohc
-    
-    return weight, target, context
+    ohe = np.array([sym_codes.index(sym) for sym in seq])
+    ohe = tf.keras.utils.to_categorical(ohe, num_classes=len(sym_codes), dtype='int32')
+    return ohe
 
 
-# # Generator Model
+def OHE_to_seq(ohe):
+    pass
 
-# In[7]:
 
-
-# make generative model
+# MODELS
+# Generator Model
 def make_generative_model():
-    """
-    Makes generative generative model for DCGAN based off of architecture from "Protein Loop Modeling Using 
-    Deep Generative Adversarial Network" paper. 
-    
-    :return: model instance of generative model 
-    
-    """
-    
-    # convolution 
-    model = tensorflow.keras.Sequential()
-    model.add(keras.Input(shape=((180, 20))))
-    
-    model.add(keras.layers.Conv1D(8, 3, strides = 1, padding='same', name='first'))
-    model.add(keras.layers.BatchNormalization())
-    model.add(keras.layers.ReLU())
-    
-    model.add(keras.layers.Conv1D(16, 3, strides = 1, padding='same'))
-    model.add(keras.layers.BatchNormalization())
-    model.add(keras.layers.ReLU())
-    
-    model.add(keras.layers.Conv1D(32, 3, strides = 1, padding='same'))
-    model.add(keras.layers.BatchNormalization())
-    model.add(keras.layers.ReLU())
+    """Return generative generative model.
 
-    model.add(keras.layers.Conv1D(64, 3, strides = 1, padding='same'))
-    model.add(keras.layers.BatchNormalization())
-    model.add(keras.layers.ReLU())
-    
-    model.add(keras.layers.Conv1D(128, 3, strides = 1, padding='same'))
-    model.add(keras.layers.BatchNormalization())
-    model.add(keras.layers.ReLU())
-    
-    model.add(keras.layers.Conv1D(256, 3, strides = 1, padding='same'))
-    model.add(keras.layers.BatchNormalization())
-    model.add(keras.layers.ReLU())
-    
-    # deconvolution 
-    model.add(keras.layers.Conv1DTranspose(128, 3, strides = 1, padding='same'))
-    model.add(keras.layers.BatchNormalization())
-    model.add(keras.layers.ReLU())
-    
-    model.add(keras.layers.Conv1DTranspose(64, 3, strides = 1, padding='same'))
-    model.add(keras.layers.BatchNormalization())
-    model.add(keras.layers.ReLU())
-    
-    model.add(keras.layers.Conv1DTranspose(32, 3, strides = 1, padding='same'))
-    model.add(keras.layers.BatchNormalization())
-    model.add(keras.layers.ReLU())
-    
-    model.add(keras.layers.Conv1DTranspose(16, 3, strides = 1, padding='same'))
-    model.add(keras.layers.BatchNormalization())
-    model.add(keras.layers.ReLU())
-    
-    model.add(keras.layers.Conv1DTranspose(8, 3, strides = 1, padding='same'))
-    model.add(keras.layers.BatchNormalization())
-    model.add(keras.layers.ReLU())
-    
-    #FIXEME: PLAY AROUND WITH THE RATIO OF FILTER 
-    
-    model.add(keras.layers.Conv1DTranspose(20, 3, strides = 1, padding='same', activation = 'softmax'))
+    DCGAN architecture from "Protein Loop Modeling Using Deep Generative Adversarial Network."
+
+    :return: model instance of generative model
+    """
+    # TODO: EXPERIMENT WITH THE RATIO OF FILTERS FROM LAYER TO LAYER AND WINDOW SIZES
+    # Convolution
+    model = tf.keras.Sequential()
+    model.add(tf.keras.Input(shape=(180, 20)))
+
+    model.add(tf.keras.layers.Conv1D(8, 3, strides=1, padding='same', name='C1'))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.ReLU())
+
+    model.add(tf.keras.layers.Conv1D(16, 3, strides=1, padding='same', name='C2'))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.ReLU())
+
+    model.add(tf.keras.layers.Conv1D(32, 3, strides=1, padding='same', name='C3'))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.ReLU())
+
+    model.add(tf.keras.layers.Conv1D(64, 3, strides=1, padding='same', name='C4'))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.ReLU())
+
+    model.add(tf.keras.layers.Conv1D(128, 3, strides=1, padding='same', name='C5'))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.ReLU())
+
+    model.add(tf.keras.layers.Conv1D(256, 3, strides=1, padding='same', name='C6'))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.ReLU())
+
+    # Deconvolution
+    model.add(tf.keras.layers.Conv1DTranspose(128, 3, strides=1, padding='same', name='D1'))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.ReLU())
+
+    model.add(tf.keras.layers.Conv1DTranspose(64, 3, strides=1, padding='same', name='D2'))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.ReLU())
+
+    model.add(tf.keras.layers.Conv1DTranspose(32, 3, strides=1, padding='same', name='D3'))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.ReLU())
+
+    model.add(tf.keras.layers.Conv1DTranspose(16, 3, strides=1, padding='same', name='D4'))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.ReLU())
+
+    model.add(tf.keras.layers.Conv1DTranspose(8, 3, strides=1, padding='same', name='D5'))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.ReLU())
+
+    # Last layer transforms filters to probability classes
+    model.add(tf.keras.layers.Conv1DTranspose(20, 3, strides=1, padding='same', activation='softmax', name='D6'))
 
     return model
-
-
-# In[8]:
 
 
 generator = make_generative_model()
 generator.summary()
 
 
-# # Discriminator Model
-
-# In[9]:
-
-
-# make discrimator model
+# Discriminator Model
 def make_discriminator_model():
+    """Return discriminative model.
+
+    DCGAN architecture from "Protein Loop Modeling Using Deep Generative Adversarial Network."
+
+    :return: model instance of discriminative model
     """
-    Makes adverserial/discriminative model for DCGAN based off of architecture from "Protein Loop Modeling Using 
-    Deep Generative Adversarial Network" paper. 
-    
-    :return: model instance of discriminative model 
-    
-    """
-    model = tensorflow.keras.Sequential()
-    model.add(keras.Input(shape=((180, 20))))
-    
-    model.add(keras.layers.Conv1D(25, 4, strides = 2, padding='same'))
-    model.add(keras.layers.BatchNormalization())
-    model.add(keras.layers.ReLU())
-    
-    model.add(keras.layers.Conv1D(13, 4, strides = 2, padding='same'))
-    model.add(keras.layers.BatchNormalization())
-    model.add(keras.layers.ReLU())
-    
-    model.add(keras.layers.Conv1D(7, 4, strides = 2, padding='same'))
-    model.add(keras.layers.BatchNormalization())
-    model.add(keras.layers.ReLU())
-    
-    model.add(keras.layers.Conv1D(4, 4, strides = 2, padding='same'))
-    model.add(keras.layers.BatchNormalization())
-    model.add(keras.layers.ReLU())
-    
-    model.add(keras.layers.Flatten())
-    model.add(keras.layers.Dense(1, activation = 'softmax'))
-    
+    model = tf.keras.Sequential()
+    model.add(tf.keras.Input(shape=(180, 20)))
+
+    model.add(tf.keras.layers.Conv1D(25, 4, strides=2, padding='same', name='C1'))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.ReLU())
+
+    model.add(tf.keras.layers.Conv1D(13, 4, strides=2, padding='same', name='C2'))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.ReLU())
+
+    model.add(tf.keras.layers.Conv1D(7, 4, strides=2, padding='same', name='C3'))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.ReLU())
+
+    model.add(tf.keras.layers.Conv1D(4, 4, strides=2, padding='same', name='C4'))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.ReLU())
+
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(2, activation='softmax', name='dense'))
+
     return model
-
-
-# In[10]:
 
 
 discriminator = make_discriminator_model()
 discriminator.summary()
 
 
-# # Loss Function
-
-# ## Generator Loss
-
-# In[11]:
+# LOSS FUNCTIONS
+cce = tf.keras.losses.CategoricalCrossentropy()
+bce = tf.keras.losses.BinaryCrossentropy()
 
 
-cross_entropy = tensorflow.keras.losses.CategoricalCrossentropy()
+# Generator Loss
+def generator_loss(fake_output, fake_target, real_target, weight):
+    """Return generator loss.
+
+    The loss for the generator is made of reconstruction and discriminative terms
+      The reconstruction loss measures how well the generator recreates the target from the context
+      The discriminative loss measures are well the generator is fooling the discriminator
+        Remember we want the generator to recreate the target realistically (rather than taking shortcuts that minimize the reconstruction loss)
+        This means if the generator is doing well, the discriminator should think its output is real, i.e. 1
+        Hence the y_true for the discriminative loss is a tensor of 1s the length of the batch size
+    """
+    # Make y_true for calculating loss
+    true_target = tf.convert_to_tensor(real_target)
+    true_output = tf.stack([tf.zeros(fake_output.shape[0]), tf.ones(fake_output.shape[0])], axis=1)
+
+    # Apply losses
+    r_loss = cce(true_target, fake_target, weight)  # Reconstruction loss
+    d_loss = bce(true_output, fake_output)  # Discriminative loss
+    return r_loss + d_loss
 
 
-# In[12]:
+# Discriminator Loss
+def discriminator_loss(real_output, fake_output):
+    """Return discriminator loss."""
+    # Make y_true for calculating loss
+    true_real = tf.stack([tf.zeros(real_output.shape[0]), tf.ones(real_output.shape[0])], axis=1)
+    true_fake = tf.stack([tf.ones(fake_output.shape[0]), tf.zeros(fake_output.shape[0])], axis=1)
+
+    # Apply losses
+    real_loss = bce(true_real, real_output)
+    fake_loss = bce(true_fake, fake_output)
+    return real_loss + fake_loss
 
 
-def generator_loss(fake_output, generated_target, target):
-    # label: data_step[0], target: data_step[1], context: data_step[2]
-    return cross_entropy(tensorflow.ones_like(fake_output), fake_output) + cross_entropy(generated_target, target)
+# Optimizer
+generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
 
 
-# ## Discriminator Loss
-
-# In[13]:
-
-
-def discriminator_loss(real_output, fake_output, weight):
-    real_loss = cross_entropy(tensorflow.ones_like(real_output), real_output, weight)
-    fake_loss = cross_entropy(tensorflow.zeros_like(fake_output), fake_output, weight)
-    total_loss = real_loss + fake_loss
-    return total_loss
-
-
-# # Optimizer
-
-# In[14]:
-
-
-generator_optimizer = tensorflow.keras.optimizers.Adam(1e-4)
-discriminator_optimizer = tensorflow.keras.optimizers.Adam(1e-4)
-
-
-# # Training Loop
-
-# In[15]:
-
-
+# Training Loop
 def train_step(context, target, weight):
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        real_target = target  # For clearer naming :)
+        fake_target = generator(context, training=True)
+        real_output = discriminator(real_target*weight, training=True)
+        fake_output = discriminator(fake_target*weight, training=True)
 
-    with tensorflow.GradientTape() as gen_tape, tensorflow.GradientTape() as disc_tape:
-        generated_target = generator(context, training=True)*weight
-        
-        real_output = discriminator(target, training=True)
-        fake_output = discriminator(generated_target, training=True)
-        
-        gen_loss = generator_loss(fake_output, generated_target, target)
+        gen_loss = generator_loss(fake_output, fake_target, real_target, weight)
         disc_loss = discriminator_loss(real_output, fake_output)
-    
-    #backpropogration 
 
-    gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
-    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+        gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+        gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
 
-    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
-    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
-
-
-# In[17]:
+        generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+        discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
 
 
 def train(context, target, weight, epochs):
-    
-    # batch data 
-    context_batch = np.array_split(context, BATCH_SIZE)
-    target_batch = np.array_split(target, BATCH_SIZE)
-    weight_batch = np.array_split(weight, BATCH_SIZE)
-    
-    for epoch in range(epochs):
+    # Batch data
+    context_batch = np.array_split(context, BATCH_NUM)
+    target_batch = np.array_split(target, BATCH_NUM)
+    weight_batch = np.array_split(weight, BATCH_NUM)
 
-        for context, target, weight in zip(context_batch, target_batch, weight_batch):
-            
+    # Training loop
+    for epoch in range(epochs):
+        print(f'EPOCH {epoch}')
+        for batch, (context, target, weight) in enumerate(zip(context_batch, target_batch, weight_batch)):
+            print(f'\tBATCH {batch}')
             train_step(context, target, weight)
 
 
-# In[18]:
+# Load data and train
+train_seq, train_label = load_data(train_seq_path, train_label_path)
+train_context, train_weight = get_context_weight(train_seq, train_label)
 
+train(train_context, train_seq, train_weight, 10)
 
-# Load data
-train_seq, train_label = load_data('../inpainting_mobidb/out/train_seq.fasta', '../inpainting_mobidb/out/train_label.fasta')
-train_weight, train_target, train_context = get_weight_target_context(train_seq, train_label)
-
-
-#test_data = load_data('../../inpainting_mobidb/out/test_seq.fasta','../../inpainting_mobidb/out/test_label.fasta')
-
-#valid_data = load_data('../../inpainting_mobidb/out/validation_seq.fasta','../../inpainting_mobidb/out/validation_label.fasta')
-
-
-# In[19]:
-
-
-contxt = train_context[:5]
-trgt = train_target[:5]
-wght = train_weight[:5]
-
-
-# In[25]:
-
-
-contxt.shape
-
-
-# In[26]:
-
-
-trgt.shape
-
-
-# In[27]:
-
-
-wght.shape
-
-
-# In[20]:
-
-
-generator(contxt, training=True)*wght
-
-
-# In[21]:
-
-
-g = make_generative_model()
-g.predict(contxt)*wght
-
-
-# In[24]:
-
-
-train(train_context, train_target, train_weight, 1)
-
+# Examine output of trained network
+real_target = train_seq
+fake_target = generator(train_context).numpy()
+real_output = discriminator(train_seq*train_weight).numpy()
+fake_output = discriminator(fake_target*train_weight).numpy()
