517f89cfa6eeedcb65ff909f246a374ff23ab91b
Samiha Mahin <samihamahin@berkeley.edu>
2022-04-07 12:57:01 -0700

    edited inpainting_exploration for simplicity and added out file for directory

--- analysis/inpainting_mobidb/inpainting_exploration/inpainting_exploration.py
+++ analysis/inpainting_mobidb/inpainting_exploration/inpainting_exploration.py
@@ -1,45 +1,56 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[93]:
-
-
-import os
+"""Data exploration and data formatting"""
 
 import matplotlib.pyplot as plt
 import pandas as pd
 import scipy.ndimage as ndimage
 from Bio import SeqIO
 
-
-# In[94]:
-
-
+# Load data
 fasta_seq = SeqIO.parse('../../mobidb_validation/generate_fastas/out/allseq.fasta', 'fasta')
 fasta_disorder = SeqIO.parse('../../mobidb_validation/generate_fastas/out/alldisorder.fasta', 'fasta')
 
 
-# In[95]:
-
-
 def get_segments(aa_seq, label_seq, segment_type, accession, description):
-    #print(label_seq)
+    """
+    Outputs list of dictionaries for each segment with parameters
+           [ { 'accession': accession id, 'description': description, 'segment_type': segment_type,
+           'len': length, 'slice': slice, 'A': number of A in aa_seq, 'R': number of R in aa_seq ... }, ...]
+
+    :param aa_seq: string amino acid sequence
+    :param label_seq: boolean list of whether disordered or ordered (0 = order, 1 = disorder)
+    :param segment_type: 'D': disordered sequence 'O': ordered sequence 'P': entire protein
+    :param accession: string accession id
+    :param description: string description
+    :return: list of dictionaries containing parameters for each segment_type sequence in entire protein
+    """
+    # Outputs the range of ordered/disordered sequences as a slice
     slices = ndimage.find_objects(ndimage.label(label_seq)[0])
-    #print(slices)
-    ds = []
+
+    segments = []
+
     for s in slices:
+
         segment = aa_seq[s[0]]  # Unpack 1-element slice tuple
-        d = {'accession': accession, 'description': description, 
+        record = {'accession': accession, 'description': description,
              'segment_type': segment_type, 'len': len(segment), 
              'slice': s}
-        
+
+        # Merging dictionary of amino acid counts to record dictionary
         aa_counts = count_amino_acids(segment)
-        d.update(aa_counts)
-        ds.append(d)
-    return ds
+        record.update(aa_counts)
+
+        segments.append(record)
+
+    return segments
 
 
 def count_amino_acids(aa_seq):
+    """
+    Creates dictionary of counts of amino acid sequence in given aa_seq
+
+    :param aa_seq: string of amino acid sequence
+    :return: dictionary of the count of amino acids
+    """
     aa_codes = ['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H',
                 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 
                 'Y', 'V', 'O', 'U', 'B', 'Z', 'X', 'J']
@@ -49,26 +60,24 @@ def count_amino_acids(aa_seq):
     return d
 
 
-# In[96]:
-
-
+# Create dictionary with key-value pair, "accession" : "amino acid sequence"
 protein_seq_dict = {}
 for protein in fasta_seq:
     protein_seq_dict[protein.id.split("|")[0]] = str(protein.seq)
 
-
-# In[97]:
-
-
 rows = []
 for protein in fasta_disorder:
+    # Lists of booleans if label is ordered or disordered
     dis_labels = [s == '1' for s in protein.seq]
     ord_labels = [s == '0' for s in protein.seq]
 
+    # Obtaining amino acid sequence from protein_seq_dict
     accession = protein.id.split("|")[0]
-    description = protein.description.split("|")[-1]
     aa_seq = protein_seq_dict[accession]
-    
+
+    # Obtaining description for get_segments parameter
+    description = protein.description.split("|")[-1]
+
     # Disordered regions have the code 'D' and ordered regions have the code 'O'
     # The entire protein is added with the code 'P'
     ds_dis = get_segments(aa_seq, dis_labels, 'D', accession, description)
@@ -79,76 +88,49 @@ for protein in fasta_disorder:
     rows.extend(ds_dis)
     rows.extend(ds_ord)
     rows.extend(ds_all)
+
+# Create dataframe with information of disordered segment, ordered segments, and full protein
 df1 = pd.DataFrame(rows)
-#print(df1)
 
 
-# In[98]:
-
-
-#df1
-
-
-# # Length Distribution of Disordered Regions in Proteins
-
-# In[99]:
-
-
+# Length Distribution of Disordered Regions in Proteins
 disorder = df1[df1['segment_type'] == 'D']
 plt.hist(disorder['len'], bins=50)
 plt.yscale('log')
 plt.ylabel('Number of entries')
 plt.xlabel('Number of Amino Acids')
 plt.title('Length of Disordered Regions')
+plt.savefig('out/len_disorder.png')
 
-
-# ## Length of Disordered Regions Upper Limit
-
-# Length of the disordered regions drops off significantly after about a length of 90, making the upper limit of the length of our data amino acid sequence be 180, because we want >50% unmasked (amino acid sequences of the ordered regions). 
-
-# In[100]:
-
-
+# Length of the disordered regions drops off significantly after about a length of 90, making the upper limit of the
+# length of our data amino acid sequence be 180, because we want >50% unmasked (amino acid sequences of the ordered
+# regions).
 disless100 = disorder[disorder['len'] <= 100]
 plt.hist(disless100['len'])
+plt.savefig('out/len_upper_limit_disorder.png')
 
-
-# The cut off for a disordered region is more than 30 amino acid residues. And we want the max disordered region length to be 90 amino acids, as stated above. This gives us 5,619 proteins for our dataset
-
-# In[101]:
-
-
+# The cut off for a disordered region is more than 30 amino acid residues. And we want the max disordered region
+# length to be 90 amino acids, as stated above.
 dismore30_less90 = disorder.loc[(disorder['len'] >= 30) & (disorder['len'] <= 90)]
 plt.hist(dismore30_less90['len'])
+plt.savefig('out/len_lower_upper_limit_disorder.png')
 
-
-# In[102]:
-
-
+# Number of unique proteins in dataset
+# This gives us 5,619 proteins for our dataset
 dismore30_less90['accession'].nunique()
-
-
-# In[103]:
-
-
-#dismore30_less90
-
-
-# In[111]:
-
 
 # Boolean List of whether there is enough context for amino acid sequence 
 enough_context = []
 # variable of residue length desired 
 residue_len = 180
 
-# List of all possible protein acccession and slice objects
+# List of all possible protein accession and slice objects
 acc_lst = list(dismore30_less90['accession'])
 slice_lst = list(dismore30_less90['slice'])
 
 for i in range(0, len(acc_lst)):
     acc = acc_lst[i]
-    # 
+
     _slice = slice_lst[i][0]
     full_seq = protein_seq_dict[acc]
     dis_len = len(full_seq[_slice])
@@ -162,8 +144,4 @@ for i in range(0, len(acc_lst)):
     enough_context.append(cond)
 
 
-# In[114]:
-
-
 print('minimum number of entries in dataset: ' + str(sum(enough_context)))
-
