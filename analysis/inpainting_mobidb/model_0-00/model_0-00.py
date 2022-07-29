c5a6c38935fcb3e2e68feabbc6078d90ee9d861a
Marc Singleton <marcsingleton@berkeley.edu>
2022-07-29 15:05:57 -0700

    Updated model_0-00.py to use utility functions in src/

--- analysis/inpainting_mobidb/model_0-00/model_0-00.py
+++ analysis/inpainting_mobidb/model_0-00/model_0-00.py
@@ -2,71 +2,10 @@
 
 import os
 
-import Bio.SeqIO as SeqIO
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-
-
-# Utility functions
-def load_data(seq_path, label_path):
-    """Return sequences and labels from FASTA files as arrays.
-
-    :param seq_path: path for FASTA file of amino acid sequences
-    :param label_path: path for FASTA file of labels of amino acid sequences where disordered residues are labeled as 1
-        and ordered residues are labeled as 0
-    :return: array of one hot encoded sequences and array of labels
-    """
-    ohes = []
-    labels = []
-    for record_seq, record_label in zip(SeqIO.parse(seq_path, 'fasta'), SeqIO.parse(label_path, 'fasta')):
-        # Convert string seq to one-hot-encoded seq
-        ohe = seq_to_OHE(str(record_seq.seq))
-        ohes.append(ohe)
-
-        # Convert string label to int label
-        label = [int(sym) for sym in record_label]
-        labels.append(label)
-
-    return np.array(ohes), np.array(labels)
-
-
-def get_context_weight(ohe, label):
-    """Return context and weight from one-hot-encoded sequences and labels.
-
-    :param ohe: one hot ended 2D arrays of sequences
-    :param label: array of labels corresponding to seq_ohc
-    :return: context and weight according to ohc and label
-    """
-    weight = np.expand_dims(label, axis=2)  # weight is binary classification of data (1:target 0: context)
-    context = (np.invert(weight) + 2) * ohe  # context is the opposite of the weight (0:target 1: context)
-
-    return context, weight
-
-
-def seq_to_OHE(seq):
-    """Return amino acid sequence as one-hot-encoded vector.
-
-    :param seq: amino acid sequence as string
-    :return: one-hot-encoded string as 2D array
-    """
-    ohe = np.array([sym2idx[sym] for sym in seq])
-    ohe = tf.keras.utils.to_categorical(ohe, num_classes=len(alphabet), dtype='int32')
-    return ohe
-
-
-def OHE_to_seq(ohe):
-    """Return one-hot-encoded vector as amino acid sequence.
-
-    :param ohe: one-hot-encoded string as 2D array
-    :return: amino acid sequence as list
-    """
-    x = np.argmax(ohe, axis=1)
-    seq = []
-    for idx in x:
-        sym = alphabet[idx]
-        seq.append(sym)
-    return seq
+from src.inpainting_mobidb.utils import load_data, get_context_weight
 
 
 # MODELS
@@ -331,8 +270,8 @@ train_label_path = '../split_data/out/train_labels.fasta'
 valid_seq_path = '../split_data/out/validation_seqs.fasta'
 valid_label_path = '../split_data/out/validation_labels.fasta'
 
-train_seq, train_label = load_data(train_seq_path, train_label_path)
-valid_seq, valid_label = load_data(valid_seq_path, valid_label_path)
+train_seq, train_label = load_data(train_seq_path, train_label_path, sym2idx)
+valid_seq, valid_label = load_data(valid_seq_path, valid_label_path, sym2idx)
 
 train_context, train_weight = get_context_weight(train_seq, train_label)
 valid_context, valid_weight = get_context_weight(valid_seq, valid_label)
