878ddf44c5f5a0273d9cf7cdfd84c6fab3a11bb7
Marc Singleton <marcsingleton@berkeley.edu>
2023-03-16 12:15:27 -0700

    Added optional add_records argument to BatchGenerator

--- src/models/utils.py
+++ src/models/utils.py
@@ -1,6 +1,6 @@
 """Utilities for training models."""
 
-from math import floor
+from math import ceil, floor
 
 import numpy as np
 import tensorflow as tf
@@ -11,11 +11,12 @@ class BatchGenerator(tf.keras.utils.Sequence):
     """Label, batch, and pad protein sequence data.
 
     Only complete batches are returned, so a single epoch may not train on every example."""
-    def __init__(self, records, batch_size, alphabet, weights, shuffle=True):
+    def __init__(self, records, batch_size, alphabet, weights, shuffle=True, all_records=False):
         self.records = records
         self.indices = np.arange(len(self.records))
         self.batch_size = batch_size
         self.shuffle = shuffle
+        self.all_records = all_records
         self.sym2idx = {}
         for idx, sym in enumerate(alphabet):
             self.sym2idx[sym] = idx
@@ -27,16 +28,22 @@ class BatchGenerator(tf.keras.utils.Sequence):
 
     def __len__(self):
         """Return number of batches."""
-        return floor(len(self.records) / self.batch_size)
+        if self.all_records:
+            return ceil(len(self.records) / self.batch_size)
+        else:
+            return floor(len(self.records) / self.batch_size)
 
     def __getitem__(self, index):
         """Generate one batch of data."""
+        if index >= len(self):
+            raise IndexError('batch index out of range')
         indices = self.indices[index*self.batch_size:(index+1)*self.batch_size]
         records = [self.records[i] for i in indices]
+        batch_size = len(records)
         max_len = max([len(record[0]) for record in records])
-        x = np.zeros((self.batch_size, max_len))
-        y = np.zeros((self.batch_size, max_len))
-        w = np.zeros((self.batch_size, max_len))
+        x = np.zeros((batch_size, max_len))
+        y = np.zeros((batch_size, max_len))
+        w = np.zeros((batch_size, max_len))
         for i, (syms, labels) in enumerate(records):
             x[i, :len(syms)] = [self.sym2idx.get(sym, 0) for sym in syms]
             y[i, :len(syms)] = [self.label2idx.get(label, 0) for label in labels]
